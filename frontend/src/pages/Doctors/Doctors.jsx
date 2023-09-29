81e560a25fc6042f790f7f98632fc4cf403a7f09
Anikesh02 <anikeshkulalinft-@athartvacoe.ac.in>
2023-09-29 17:00:39 +0530

    git commit -m update"

--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import DoctorCard from './../../components/Doctors/DoctorCard'
+import {doctors} from './../../assets/data/doctors'
 
 const Doctors = () => {
-  return  <div>Doctors</div>
+  return  <>
+  <section className='bg-[#fff9ea]'>
+    <div className="container text-center">
+      <h2 className='heading'>Find a Doctor</h2>
+      <div className="max-w-[570px] mt-[30px] mx-auto bg-[#0066ff2c] rounded-md flex items-center justify-between">
+        <input type="search" className="py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer placeholder:text-textColor" placeholder="Search Doctor" />
+      </div>
+    </div>
+  </section>
+  </>
   
 }
 
