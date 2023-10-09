82a67da8ffbc690bf550d0c84902c0481e89371e
Abk0710 <muruganabhishekinft-@atharvacoe.ac.in>
2023-10-09 22:58:54 +0530

    services descriptions

--- frontend/src/assets/data/services.js
+++ frontend/src/assets/data/services.js
@@ -1,37 +1,37 @@
 export const services = [
   {
     name: "Cancer Care",
-    desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+    desc: "Surgery, radiation, chemotherapy and hormone therapy are available for the relieve from Cancer.",
     bgColor: "rgba(254, 182, 13, .2)",
     textColor: "#FEB60D",
   },
   {
     name: "Dialysis",
-    desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+    desc: "Top class service to remove waste products and excess fluid from the blood when the kidneys stop working properly.",
     bgColor: "rgba(151, 113, 255, .2)",
     textColor: "#9771FF",
   },
   {
     name: "Heart & Vascular",
-    desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+    desc: "Best Treatment for your heart by Top Surgeons in India.",
     bgColor: "rgba(1, 181, 197, .2)",
     textColor: "#01B5C5",
   },
   {
     name: "Mental Health",
-    desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+    desc: "Best care for how we think, feel, and act. It also helps determine how we handle stress, relate to others, and make healthy choices.",
     bgColor: "rgba(1, 181, 197, .2)",
     textColor: "#01B5C5",
   },
   {
     name: "Neurology",
-    desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+    desc: "Specialized treatment for disorders that affect the brain, spinal cord, and nerves, such as: Cerebrovascular disease, such as stroke.",
     bgColor: "rgba(254, 182, 13, .2)",
     textColor: "#FEB60D",
   },
   {
     name: "Burn Treatment",
-    desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+    desc: "Immediate and Effective Remedies and Therapies for Burns and septic wounds.",
     bgColor: "rgba(151, 113, 255, .2)",
     textColor: "#9771FF",
   },
