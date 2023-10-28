5a284a2f3f6bc8064a5624cc047dd3aef0f5f46b
Anikesh02 <anikeshkulalinft-@atharvacoe.ac.in>
2023-10-28 11:35:21 +0530

    Home Updated

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,12 @@
 import React from 'react'
 import heroImg01 from '../assets/images/hero-img01.png'
-// import heroImg02 from '../assets/images/hero-img02.png'
-// import heroImg03 from '../assets/images/hero-img03.png'
 import icon01 from '../assets/images/icon01.png'
 import icon02 from '../assets/images/icon02.png'
 import icon03 from '../assets/images/icon03.png'
 import featureImg from '../assets/images/feature-img.jpg'
-// import videoIcon from '../assets/images/video-icon.png'
-import avatarIcon from '../assets/images/avatar-icon.png'
 import faqImg from '../assets/images/faq-img.png'
 import { Link } from 'react-router-dom'
 import { BsArrowRight } from 'react-icons/bs'
-import About from '../components/About/About'
-import ServiceList from '../components/Services/ServiceList'
 import DoctorList from '../components/Hospitals/HospitalList'
 import FaqList from '../components/Faq/FaqList'
 import Testimonial from '../components/Testimonials/Testimonial'
@@ -61,7 +55,7 @@ const Home = () => {
                 <h1 className='text-[36px] leading-[46px] text-headingColor font-[800] md:text-[40px] md:leading-[70px]'>Hello, {user?.name ? user.name : 'User'}.</h1>
                 <h1 className='text-[36px] leading-[46px] text-headingColor font-[800] md:text-[40px] md:leading-[70px]'>We help our elders live a healthy, longer life.</h1>
                 <p className='text__para'>
-                EldCare was founded on a better model of care one designed around patients needs that provides a higher level of quality and service affordably. We do this through innovative design, excellent customer service, and the efficient use of technology.</p>
+                  EldCare was founded on a better model of care one designed around patients needs that provides a higher level of quality and service affordably. We do this through innovative design, excellent customer service, and the efficient use of technology.</p>
                 <Link to='/appointment'><button className='btn'>Request an Appointment</button></Link>
                 <Link to='/care'><button className='btn ml-5'>Apply for Care Taker</button></Link>
 
@@ -96,12 +90,7 @@ const Home = () => {
               <div>
                 <img className='w-full' src={heroImg01} alt="" />
               </div>
-              {/* <div className='mt-[30px]'>
-                <img src={heroImg02} alt="" className='w-full mb-[30px]' />
-                <img src={heroImg03} alt="" className='w-full' />
-
-
-              </div> */}
+             
             </div>
 
           </div>
@@ -192,37 +181,14 @@ const Home = () => {
             {/* Feature img  */}
             <div className="relative z-10 xl:w-[770px] flex justify-end mt-[50px] lg:mt-0 items-center">
               <img src={featureImg} className='w-full items-center justify-between' alt="" />
-              {/* <div className="w-[150px] lg:w-[248px] bg-white absolute bottom-[50px] left-0 md:bottom-[100px] md:left-5 z-20 p-2 pb-3 lg:pt-4 lg:px-4 lg:pb-[26px] rounded-[10px]">
-                <div className="flex items-center justify-between"> */}
-                  {/* <div className="flex items-center gap-[6px] lg:gap-3">
-                    <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-headingColor font-[600] ">Tue, 24</p>
-                    <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-textColor font-[400] ">10:00 AM</p> */}
-                    {/* <div>
-                      <span className="w-5 h-5 lg:w-[34px] lg:h-[34px] flex items-center justify-center bg-yellowColor rounded py-1 px-[6px] lg:py-3 lg:px-[9px]">
-                        <img src={videoIcon} alt="" />
-                      </span>
-                    </div> */}
-                  {/* </div>
-                </div> */}
-
-                {/* <div className="w-[65px] lg:w-[96px] bg-[#CCF0F3] py-1 px-2 lg:py-[6px] lg:px-[10px] text-[8px] leading-[8px] lg:text-[12px] lg:leading-4 text-irisBlueColor font-[500] mt-2 lg:mt-4 rounded-full">
-                  Consultation
-                </div> */}
-
-                {/* <div className="flex items-center gap-[6px] lg:gap-[10px] mt-2 lg:mt-[18px]">
-                  <img src={avatarIcon} alt="" />
-                  <h4 className="text-[10px] leading-3 lg:text-[16px] lg:leading-[22px] font-[700] text-headingColor">
-                    Anikesh Kulal
-                  </h4>
-                </div> */}
+              
 
 
-              </div>
             </div>
+          </div>
           {/* </div> */}
         </div>
       </section>
-      {/* Feature Section End */}
 
       {/* Great Doctors  */}
       <section>
