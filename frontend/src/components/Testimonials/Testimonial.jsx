3c097abfc39892d98f28c65b2f511b298e2ce595
Anikesh02 <anikeshkulalinft-@atharvacoe.ac.in>
2023-10-28 10:00:34 +0530

    Testimonial updated

--- frontend/src/components/Testimonials/Testimonial.jsx
+++ frontend/src/components/Testimonials/Testimonial.jsx
@@ -4,6 +4,9 @@ import {Swiper, SwiperSlide} from 'swiper/react'
 import 'swiper/css'
 import 'swiper/css/pagination'
 import patientAvatar from '../../assets/images/patient-avatar.png'
+import profile1 from '../../assets/images/people1.png'
+import profile2 from '../../assets/images/people2.png'
+import profile3 from '../../assets/images/people3.png'
 import {HiStar} from 'react-icons/hi'
 
 const Testimonial = () => {
@@ -27,7 +30,26 @@ const Testimonial = () => {
             <SwiperSlide>
                 <div className="py-[30px] px-5 rounded-3">
                     <div className="flex items-center gap-[13px]">
-                        <img src={patientAvatar} alt="" />
+                        <img src={profile3} alt="" />
+                        <div>
+                            <h4 className='text-[18px] leading-[30px] font-semibold text-headingColor'>Isha Gupta</h4>
+                            <div className="flex items-center gap-[2px]">
+                                <HiStar className='text-yellowColor w-[18px] h-5'/>
+                                <HiStar className='text-yellowColor w-[18px] h-5'/>
+                                <HiStar className='text-yellowColor w-[18px] h-5'/>
+                                <HiStar className='text-yellowColor w-[18px] h-5'/>
+                                <HiStar className='text-yellowColor w-[18px] h-5'/>
+                            </div>
+                        </div>
+                    </div>
+                    <p className='text-[16px] leading-7 mt-4 text-textColor font-[400]'>I have taken medical services from them. They treat so well and they are providing the best medical services.</p>
+                </div>
+            </SwiperSlide>
+
+            <SwiperSlide>
+                <div className="py-[30px] px-5 rounded-3">
+                    <div className="flex items-center gap-[13px]">
+                        <img src={profile1} alt="" />
                         <div>
                             <h4 className='text-[18px] leading-[30px] font-semibold text-headingColor'>Muhibur Rahman</h4>
                             <div className="flex items-center gap-[2px]">
@@ -46,26 +68,7 @@ const Testimonial = () => {
             <SwiperSlide>
                 <div className="py-[30px] px-5 rounded-3">
                     <div className="flex items-center gap-[13px]">
-                        <img src={patientAvatar} alt="" />
-                        <div>
-                            <h4 className='text-[18px] leading-[30px] font-semibold text-headingColor'>Muhibur Rahman</h4>
-                            <div className="flex items-center gap-[2px]">
-                                <HiStar className='text-yellowColor w-[18px] h-5'/>
-                                <HiStar className='text-yellowColor w-[18px] h-5'/>
-                                <HiStar className='text-yellowColor w-[18px] h-5'/>
-                                <HiStar className='text-yellowColor w-[18px] h-5'/>
-                                <HiStar className='text-yellowColor w-[18px] h-5'/>
-                            </div>
-                        </div>
-                    </div>
-                    <p className='text-[16px] leading-7 mt-4 text-textColor font-[400]'>I have taken medical services from them. They treat so well and they are providing the best medical services.</p>
-                </div>
-            </SwiperSlide>
-
-            <SwiperSlide>
-                <div className="py-[30px] px-5 rounded-3">
-                    <div className="flex items-center gap-[13px]">
-                        <img src={patientAvatar} alt="" />
+                        <img src={profile2} alt="" />
                         <div>
                             <h4 className='text-[18px] leading-[30px] font-semibold text-headingColor'>Muhibur Rahman</h4>
                             <div className="flex items-center gap-[2px]">
