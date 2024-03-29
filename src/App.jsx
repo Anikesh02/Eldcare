a1941afc132b61bedc089f4285f3de4be629cb1b
Anikesh02 <anikeshkulalinft-@atharvacoe.ac.in>
2024-03-29 10:00:10 +0530

    Linked Matomo

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,16 @@ import './App.css';
 import Layout from './layout/Layout';
 import Home from './pages/Home';
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import React from 'react';
 
 function App() {
+
+  React.useEffect(() => {
+    var _mtm = window._mtm = window._mtm || [];
+    _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src='https://cdn.matomo.cloud/eldcarevercelapp.matomo.cloud/container_GJPYXTlG.js'; s.parentNode.insertBefore(g,s);
+   }, [])
  
   return(
     
@@ -15,3 +23,8 @@ function App() {
 }
 
 export default App
+
+
+
+
+
